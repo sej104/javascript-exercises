ec2eb7b723964529c798c7e0c4c9237dda300c0d
Sergio Joya <sejoya104@gmail.com>
2024-07-11 08:27:26 -0400

    Add 05_sumAll exercise solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number" ) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let index = num1;
+    let condition = num2; 
+
+    if (num1 > num2) {
+        index = num2;
+        condition = num1;
+    } 
+
+    for (index; index <= condition; index++) {
+        sum += index;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
