1bbf9b356a670e1aa7045855c4785e63943f459a
Sergio Joya <sejoya104@gmail.com>
2024-07-23 16:06:51 -0400

    Add 09-palindromes exercise solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const array = str.split("");
+    for (let i = array.length - 1; i > 0; i--) {
+        let value = array[i];
+        if ( (value.toUpperCase() === value.toLowerCase() ) &&
+            isNaN(value) || value === " " ) {
+                array.splice(i, 1);
+            }
+    }
+    const nonReversedString = array.join("").toLowerCase();
+    const reversedString = array.reverse().join("").toLowerCase();
+    return nonReversedString === reversedString;
 };
 
 // Do not edit below this line
